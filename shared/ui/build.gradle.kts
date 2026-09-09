05dd3d2b9a840242d14dbda03990910c97329e86
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-09 08:44:18 -0300

    build: add Material UI

--- shared/ui/build.gradle.kts
+++ shared/ui/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
             implementation(project(":shared:core"))
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
+            implementation(libs.compose.material)
         }
     }
 }

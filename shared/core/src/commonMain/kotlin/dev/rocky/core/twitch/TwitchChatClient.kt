3bf266af3015b2f6629159642fd8842f0f84c81d
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-09 15:04:30 -0300

    feat: define Twitch chat contract

---
+++ shared/core/src/commonMain/kotlin/dev/rocky/core/twitch/TwitchChatClient.kt
@@ -0,0 +1,46 @@
+package dev.rocky.core.twitch
+
+import dev.rocky.core.live.ChatMessage
+
+enum class TwitchConnectionPhase {
+    Disconnected,
+    Authenticating,
+    AwaitingAuthorization,
+    Connecting,
+    Connected,
+    Reconnecting,
+    Failed,
+}
+
+data class TwitchAccount(
+    val userId: String,
+    val login: String,
+)
+
+sealed interface TwitchConnectionEvent {
+    data class PhaseChanged(
+        val phase: TwitchConnectionPhase,
+        val detail: String? = null,
+    ) : TwitchConnectionEvent
+
+    data class AuthorizationRequired(
+        val userCode: String,
+        val verificationUri: String,
+    ) : TwitchConnectionEvent
+
+    data class Connected(val account: TwitchAccount) : TwitchConnectionEvent
+
+    data class MessageReceived(val message: ChatMessage) : TwitchConnectionEvent
+}
+
+fun interface TwitchConnectionListener {
+    fun onEvent(event: TwitchConnectionEvent)
+}
+
+interface TwitchChatClient : AutoCloseable {
+    fun connect(clientId: String, listener: TwitchConnectionListener)
+
+    fun disconnect()
+
+    override fun close()
+}

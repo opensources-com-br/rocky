16c508007bf16751c94d1dc3641829d79bda728f
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-09 08:32:41 -0300

    build: configure data module

---
+++ shared/data/build.gradle.kts
@@ -0,0 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm("desktop") {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":shared:core"))
+        }
+    }
+}

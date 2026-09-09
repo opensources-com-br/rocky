d3e54925e097a02b84ad80cf7ef2461f585233e0
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-09 08:33:25 -0300

    feat: add desktop entry point

---
+++ apps/desktop/src/desktopMain/kotlin/dev/rocky/app/Main.kt
@@ -0,0 +1,11 @@
+package dev.rocky.app
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Rocky",
+    ) {}
+}

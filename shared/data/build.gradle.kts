fe0481f1170168b8ba3aaba34db245434df1e5f8
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-09 14:37:41 -0300

    build: add SQLDelight persistence

--- shared/data/build.gradle.kts
+++ shared/data/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.sqldelight)
 }
 
 kotlin {
@@ -12,6 +13,20 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(project(":shared:core"))
+            implementation(libs.sqldelight.runtime)
+        }
+        val desktopMain by getting {
+            dependencies {
+                implementation(libs.sqldelight.sqlite.driver)
+            }
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("RockyDatabase") {
+            packageName.set("dev.rocky.data.db")
         }
     }
 }

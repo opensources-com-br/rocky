279d430041fd36b74063b8c3a095abe533a71b92
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-11 13:43:14 -0300

    test: identify the chat message list

--- shared/ui/src/commonMain/kotlin/dev/rocky/ui/window/ConversationContent.kt
+++ shared/ui/src/commonMain/kotlin/dev/rocky/ui/window/ConversationContent.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -101,7 +102,7 @@ internal fun ConversationContent(
             )
         } else {
             LazyColumn(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier.weight(1f).testTag("chat-messages"),
                 state = chatScrollState,
                 verticalArrangement = Arrangement.spacedBy(14.dp),
             ) {

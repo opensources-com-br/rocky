ab5a99c47d7eade67a79c018d6fe56443b1bf667
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-12 17:04:19 -0300

    feat: implement landing LandingView

---
+++ apps/web/components/landing/LandingView.jsx
@@ -0,0 +1,23 @@
+import LandingNav from "./LandingNav";
+import HeroSection from "./HeroSection";
+import PlatformsStrip from "./PlatformsStrip";
+import HowItWorks from "./HowItWorks";
+import FeaturesSection from "./FeaturesSection";
+import PrivacySection from "./PrivacySection";
+import DownloadSection from "./DownloadSection";
+import LandingFooter from "./LandingFooter";
+
+export default function LandingView({ chord, features, guarantees, heroChips, heroConvo, heroTabs, localCards, mic, platforms, steps }) {
+  return (<>
+<div style={{"background": "#080706", "color": "#f2efec", "fontFamily": "var(--font-geist-sans), sans-serif", "overflowX": "hidden", "letterSpacing": "-.011em"}}>
+<LandingNav  />
+<HeroSection chord={chord} heroChips={heroChips} heroConvo={heroConvo} heroTabs={heroTabs} mic={mic} />
+<PlatformsStrip platforms={platforms} />
+<HowItWorks steps={steps} />
+<FeaturesSection features={features} />
+<PrivacySection guarantees={guarantees} localCards={localCards} />
+<DownloadSection  />
+<LandingFooter  />
+</div>
+  </>);
+}

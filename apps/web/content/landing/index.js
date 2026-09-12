52105a5d6246d3e3242d7013ce691e2f3a8f8969
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-12 17:02:15 -0300

    feat: port landing content index

---
+++ apps/web/content/landing/index.js
@@ -0,0 +1,8 @@
+export { heroChips } from "./heroChips";
+export { heroTabs } from "./heroTabs";
+export { heroConvo } from "./heroConvo";
+export { steps } from "./steps";
+export { platforms } from "./platforms";
+export { features } from "./features";
+export { localCards } from "./localCards";
+export { guarantees } from "./guarantees";

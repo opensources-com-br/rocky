ba0e410e341457b48eba7fe9979d9af9ee225420
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-12 16:49:03 -0300

    feat: prepare Next.js landing for static hosting

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={100}
           height={20}
@@ -47,7 +48,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
+              src={`${basePath}/vercel.svg`}
               alt="Vercel logomark"
               width={16}
               height={14}

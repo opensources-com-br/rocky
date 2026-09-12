c830ca8b25858712ff3d5772326cca911230df7f
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-12 17:02:15 -0300

    feat: port landing content platforms

---
+++ apps/web/content/landing/platforms.js
@@ -0,0 +1,6 @@
+export const platforms = [
+  {"name": "Twitch", "color": "#9146ff"},
+  {"name": "Kick", "color": "#53fc18"},
+  {"name": "YouTube", "color": "#ff0033"},
+  {"name": "Facebook", "color": "#1877f2"},
+];

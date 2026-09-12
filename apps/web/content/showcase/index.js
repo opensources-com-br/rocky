1cdd271bf8c76dea6278445272b94708818d3c0d
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-12 17:02:15 -0300

    feat: port showcase content index

---
+++ apps/web/content/showcase/index.js
@@ -0,0 +1,11 @@
+import julia from "./julia";
+import marcos from "./marcos";
+import canal_sete from "./canal-sete";
+import tati from "./tati";
+import oficina from "./oficina";
+import renan from "./renan";
+import mesa_de_bar from "./mesa-de-bar";
+import atelie from "./atelie";
+import bruno from "./bruno";
+export const CASES = [julia, marcos, canal_sete, tati, oficina, renan, mesa_de_bar, atelie, bruno];
+export const TW = "#9146ff", KI = "#53fc18", YT = "#ff0033", FB = "#1877f2";

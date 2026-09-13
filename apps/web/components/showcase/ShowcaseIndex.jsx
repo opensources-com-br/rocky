0cd1f663f0083b3f2087f0677fa1ff16e8fc93aa
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-13 19:16:32 -0300

    feat: pass locale to Showcase sections

--- apps/web/components/showcase/ShowcaseIndex.jsx
+++ apps/web/components/showcase/ShowcaseIndex.jsx
@@ -1,11 +1,11 @@
 import ShowcaseHeading from "./ShowcaseHeading";
 import ContributeCallout from "./ContributeCallout";
 
-export default function ShowcaseIndex({ isIndex }) {
+export default function ShowcaseIndex({ isIndex, locale }) {
   if (!isIndex) return null;
   return <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-    <ShowcaseHeading />
+    <ShowcaseHeading locale={locale} />
     {/* Filtros e grade de casos serão reativados junto com os use cases. */}
-    <ContributeCallout />
+    <ContributeCallout locale={locale} />
   </div>;
 }

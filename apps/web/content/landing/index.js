28ef414737e952216886b6ce773294fdc2ea2099
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-13 19:13:30 -0300

    feat: select landing content by locale

--- apps/web/content/landing/index.js
+++ apps/web/content/landing/index.js
@@ -1,5 +1,15 @@
-export { steps } from "./steps";
-export { platforms } from "./platforms";
-export { features } from "./features";
-export { localCards } from "./localCards";
-export { guarantees } from "./guarantees";
+import { getSteps } from "./steps";
+import { getPlatforms } from "./platforms";
+import { getFeatures } from "./features";
+import { getLocalCards } from "./localCards";
+import { getGuarantees } from "./guarantees";
+
+export function getLandingContent(locale) {
+  return {
+    steps: getSteps(locale),
+    platforms: getPlatforms(locale),
+    features: getFeatures(locale),
+    localCards: getLocalCards(locale),
+    guarantees: getGuarantees(locale),
+  };
+}

510e92ef47c538b616f1766c17888803507ba074
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-15 20:11:29 -0300

    test: abort oversized fragmented Twitch messages

--- shared/data/src/desktopTest/kotlin/dev/rocky/data/twitch/TwitchWebSocketListenerTest.kt
+++ shared/data/src/desktopTest/kotlin/dev/rocky/data/twitch/TwitchWebSocketListenerTest.kt
@@ -4,6 +4,16 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TwitchWebSocketListenerTest {
+    @Test fun abortsOversizedFragmentsAndReportsFailure() {
+        val socket = FakeTwitchSocket()
+        var failures = 0
+        val listener = TwitchWebSocketListener({}, { _, _ -> }, { _, _ -> failures++ }, maxMessageChars = 8)
+        listener.onText(socket, "12345", false)
+        listener.onText(socket, "6789", true)
+        assertEquals(true, socket.aborted)
+        assertEquals(1, failures)
+    }
+
     @Test fun parsesFragmentedKeepaliveOnceAndRequestsMoreFrames() {
         val events = mutableListOf<TwitchSocketEvent>()
         val socket = FakeTwitchSocket()

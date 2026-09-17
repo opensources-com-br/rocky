b7981e428ea48d64ee2f4b50db942da6141ba088
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-17 20:11:22 -0300

    test: clean offline update downloads

--- shared/data/src/desktopTest/kotlin/dev/rocky/data/updates/UpdateDownloaderTest.kt
+++ shared/data/src/desktopTest/kotlin/dev/rocky/data/updates/UpdateDownloaderTest.kt
@@ -36,4 +36,5 @@ class UpdateDownloaderTest {
     @Test fun rejectsOversizedDownload() = runDownload("abcd")
     @Test fun removesTamperedDownload() = runDownload("abc", "0".repeat(64))
     @Test fun removesCancelledDownload() = runDownload("abc", cancel = true)
+    @Test fun removesOfflineDownload() = runDownload("abc", offline = true)
 }

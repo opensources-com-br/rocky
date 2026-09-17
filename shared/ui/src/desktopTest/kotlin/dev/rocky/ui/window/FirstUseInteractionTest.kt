bbcf9e76b61dee94501dfa89e02301a28a9501d5
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-16 23:22:57 -0300

    test: prepare onboarding interaction suite

---
+++ shared/ui/src/desktopTest/kotlin/dev/rocky/ui/window/FirstUseInteractionTest.kt
@@ -0,0 +1,11 @@
+package dev.rocky.ui.window
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.v2.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class FirstUseInteractionTest {
+    @get:Rule val rule = createComposeRule()
+}

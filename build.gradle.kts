d1ab534abcf75438d85c1dc5949f89fee6e25009
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-16 22:41:07 -0300

    build: enable coverage in JVM modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
+    alias(libs.plugins.kover)
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.compose) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlinx.kover")
 }
 
 allprojects {

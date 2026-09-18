1d32dbc68ca260eb92c4639c157909b55ae19c4d
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-18 18:57:46 -0300

    feat: implement updates in layout.tsx (part 2)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><LocaleProvider>{children}</LocaleProvider></body>
+      <body className="min-h-full flex flex-col"><LocaleProvider>{children}<SiteUpdateNotice /></LocaleProvider></body>
     </html>
   );
 }

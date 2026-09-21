7ca6f33bc8e22d6341d0e3f4c9cf9020080b2f8b
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-20 21:40:14 -0300

    feat: pass locale to docs navigation

--- apps/web/components/docs/DocsColumns.jsx
+++ apps/web/components/docs/DocsColumns.jsx
@@ -6,7 +6,7 @@ import DocsToc from "./DocsToc";
 export default function DocsColumns({ blocks, goNext, goPrev, hasNext, hasPrev, locale, nav, nextTitle, pageGroup, pageLead, pageTitle, prevTitle, toc }) {
   return (<>
 <SiteFrame className="docs-columns">
-<DocsSidebar nav={nav} pageTitle={pageTitle} />
+<DocsSidebar locale={locale} nav={nav} pageTitle={pageTitle} />
 <DocsArticle blocks={blocks} goNext={goNext} goPrev={goPrev} hasNext={hasNext} hasPrev={hasPrev} locale={locale} nextTitle={nextTitle} pageGroup={pageGroup} pageLead={pageLead} pageTitle={pageTitle} prevTitle={prevTitle} />
 <DocsToc locale={locale} toc={toc} />
 </SiteFrame>
